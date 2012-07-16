ce77e0cb70768fe219f147bc081ce65001c6299d
Christoffer Artmann <Artgaard@gmail.com>
2012-07-16 22:27:09 +0200

    Added Model->Load

--- library/model.php
+++ library/model.php
@@ -3,7 +3,7 @@
 class Model
 {
     public $id;
-    public $dbname;
+    public $table_name;
     
     public function __construct($data) 
     {
@@ -11,9 +11,22 @@ class Model
             $this->$key = $value;
     }
     
-    public static function Load($id)
+    public static function Load($id, $database, $table_name)
     {
+        $sql = "SELECT * FROM :table WHERE id=:id";
+        $data = array(":id" => (int)$id, ":table" => table_name);
         
+        $res = Database::Query($sql, $data, $database);
+        
+        $data = array();
+        
+        while ($row = $res->fetch(PDO::FETCH_ASSOC))
+        {
+            foreach($row as $key => $value)
+                $data[$key] = $value;
+        }
+        
+        return new Model($data);
     }
     
     public static function Create($data)
