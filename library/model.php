94096803c91a278aefe7cc10e99f32d6b9c114e5
Christoffer Artmann <Artgaard@gmail.com>
2012-07-16 22:19:11 +0200

    Added structure of model

--- library/model.php
+++ library/model.php
@@ -1,2 +1,33 @@
 <?php
 
+class Model
+{
+    public $id;
+    public $dbname;
+    
+    public function __construct() 
+    {
+        
+    }
+    
+    public static function Load($id)
+    {
+        
+    }
+    
+    public static function Create($data)
+    {
+        
+    }
+    
+    public function Update()
+    {
+        
+    }
+    
+    public function Delete()
+    {
+        
+    }
+    
+}
