01a1184936a29c5e6208e6342906b937c145ffa4
Christoffer Artmann <christoffer.artmann@glesys.se>
2012-07-25 14:47:56 +0200

    Added Controller->requiredArguments

--- library/Controller.php
+++ library/Controller.php
@@ -19,6 +19,22 @@ class Controller
         return jsponResponse($data);
     }
     
+    public function requiredArguments($data, $arguments)
+    {
+        $errors = array();
+        
+        foreach($arguments as $arg)
+        {
+            if(!isset($data[$arg]))
+                $errors[] = $arg;
+        }
+        
+        if(sizeof($errors) > 0)
+            throw new Exception(implode(", ", $errors) + " is a required field.");
+        
+    }
+
+
     public function jsonResponse($data)
     {
         return json_encode($data);
